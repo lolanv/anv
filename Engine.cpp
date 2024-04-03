815dff40dff4c99b0e918dcbd2d51fc36b3d2b54
lolanv <161153252+lolanv@users.noreply.github.com>
2024-04-03 13:55:01 +0530

    Delete Engine.cpp

--- Engine.cpp
+++
@@ -1,4 +0,0 @@
-#include "Engine.h"
-
-Engine::Engine(const std::string& engine_number, int torque, int horsepower, EngineType type)
-    : m_engine_number(engine_number), m_engine_torque(torque), m_horsepower(horsepower), m_engine_type(type) {}
